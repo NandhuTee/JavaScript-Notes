35423c37d35f574911b140034eb335f708fda3eb
Nandhini_T <rtnandhutee@gmail.com>
2024-08-31 14:15:07 +0530

    update

---
+++ interviewQuestions/Destructuring.js
@@ -0,0 +1,21 @@
+/*Destructuring is a syntax in JavaScript that allows unpacking values from arrays or properties from objects 
+into distinct variables.*/
+// Array Destructuring
+const [first, second] = [10, 20];
+console.log(first); // 10
+console.log(second); // 20
+
+// Object Destructuring
+const person = { name: 'John', age: 30 };
+const { name, age } = person;
+console.log(name); // John
+console.log(age); // 30
+
+/*Real-Time Use Case: Destructuring is commonly used in function parameters, 
+especially when dealing with objects or arrays, to directly access needed values.*/
+function greet({ name, age }) {
+    console.log(`Hello, my name is ${name} and I am ${age} years old.`);
+  }
+  
+  greet({ name: 'Alice', age: 25 });
+  
